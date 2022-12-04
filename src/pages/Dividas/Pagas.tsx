9370d57e88d2595ab0e672a57e283bf6e093b211
joao-silva09 <joao.e.silva.03@gmail.com>
2022-12-04 11:21:14 -0300

    Fix bug of dataVencimento in Pagas.tsx

--- src/pages/Dividas/Pagas.tsx
+++ src/pages/Dividas/Pagas.tsx
@@ -80,7 +80,9 @@ export default function Pagas() {
       headerName: "Data Vencimento",
       minWidth: 130,
       renderCell: (params) => {
-        return new Date(params.row.dataVencimento).toLocaleDateString();
+        return params.row.dataVencimento === "0001-01-01T00:00:00"
+          ? ""
+          : new Date(params.row.dataVencimento).toLocaleDateString() ?? "";
       },
     },
     {
