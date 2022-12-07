13a8ceb99f443b6179ae90b1e4d6cd34e5bf8cff
joao-silva09 <joao.e.silva.03@gmail.com>
2022-12-06 21:02:18 -0300

    Fix bug in DialogEditarConta.

--- src/components/Contas/DialogEditarConta.tsx
+++ src/components/Contas/DialogEditarConta.tsx
@@ -58,6 +58,7 @@ export default function DialogEditarConta({
         dto.id = data.id;
         dispatch(PutConta(dto));
         resetForm();
+        closeMenu();
         onClose();
       } catch (error: any) {
         console.log(error);
