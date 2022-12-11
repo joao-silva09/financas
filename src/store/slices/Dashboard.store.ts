ed94b11b7472c6946a914daf48920803fe4e0ebc
joao-silva09 <joao.e.silva.03@gmail.com>
2022-12-11 00:18:47 -0300

    Adiconando novas consultas na store Dashboard.

--- src/store/slices/Dashboard.store.ts
+++ src/store/slices/Dashboard.store.ts
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AxiosResponse } from "axios";
 import store, { AppDispatch, AppThunk } from "..";
 import {
   AddOperacaoDto,
@@ -14,7 +15,9 @@ const dashboardStore = createSlice({
   name: "dashboardStore",
   initialState: {
     recebimentos: 0 as number | undefined,
+    recebimentosList: {} as GetOperacaoDtoListServiceResponse,
     gastos: 0 as number | undefined,
+    gastosList: {} as GetOperacaoDtoListServiceResponse,
     filters: {
       dateFilter: new Date(),
       typeFilter: {
@@ -24,6 +27,18 @@ const dashboardStore = createSlice({
     },
   },
   reducers: {
+    setRecebimentosList(
+      state,
+      action: PayloadAction<GetOperacaoDtoListServiceResponse>
+    ) {
+      state.recebimentosList = action.payload;
+    },
+    setGastosList(
+      state,
+      action: PayloadAction<GetOperacaoDtoListServiceResponse>
+    ) {
+      state.gastosList = action.payload;
+    },
     setRecebimentos(state, action: PayloadAction<number>) {
       state.recebimentos = action.payload;
     },
@@ -35,13 +50,22 @@ const dashboardStore = createSlice({
     },
     clear(state) {
       state.recebimentos = dashboardStore.getInitialState().recebimentos;
+      state.recebimentosList =
+        dashboardStore.getInitialState().recebimentosList;
       state.gastos = dashboardStore.getInitialState().gastos;
+      state.gastosList = dashboardStore.getInitialState().gastosList;
     },
   },
 });
 
-export const { setRecebimentos, setGastos, setDashboardDateFilter, clear } =
-  dashboardStore.actions;
+export const {
+  setRecebimentos,
+  setRecebimentosList,
+  setGastosList,
+  setGastos,
+  setDashboardDateFilter,
+  clear,
+} = dashboardStore.actions;
 export default dashboardStore.reducer;
 
 export function clearDashboard(): AppThunk | any {
@@ -56,43 +80,47 @@ export function setStateDashboardDateFilter(date: Date): AppThunk | any {
   };
 }
 
-export function GetRecebimentos(): AppThunk | any {
+export function GetGastosByMonth(month: number, year: number): AppThunk | any {
   return async function (dispatch: AppDispatch | any) {
-    dispatch(
-      GetOperacoesByMonth(
-        store.getState().dashboardStore.filters.dateFilter.getMonth() + 1,
-        store.getState().dashboardStore.filters.dateFilter.getFullYear()
-      )
-    );
-    const result = store
-      .getState()
-      .operacoesStore.operacoes.data!.map((prod) =>
-        prod.tipoOperacao === TipoOperacao.Recebimento ? prod.valor : 0
-      )
-      .reduce((total, valor) => total! + valor!);
+    // dispatch(clearDashboard());
+    const result = await api
+      .get(`/api/Operacao/Get/gastos/${month}/${year}`)
+      .then((result: AxiosResponse<GetOperacaoDtoListServiceResponse>) => {
+        dispatch(setGastosList(result.data));
+        const gastosValue = store
+          .getState()
+          .dashboardStore.gastosList.data!.map((prod) =>
+            prod.tipoOperacao === TipoOperacao.Gasto ? prod.valor : 0
+          )
+          .reduce((total, valor) => total! + valor!);
 
-    dispatch(setRecebimentos(result!));
+        dispatch(setGastos(gastosValue!));
+      });
+
     return result;
   };
 }
 
-export function GetGastos(): AppThunk | any {
+export function GetRecebimentosByMonth(
+  month: number,
+  year: number
+): AppThunk | any {
   return async function (dispatch: AppDispatch | any) {
-    dispatch(
-      GetOperacoesByMonth(
-        store.getState().dashboardStore.filters.dateFilter.getMonth() + 1,
-        store.getState().dashboardStore.filters.dateFilter.getFullYear()
-      )
-    );
-    let result = store.getState().dashboardStore.gastos;
-    result = store
-      .getState()
-      .operacoesStore.operacoes.data!.map((prod) =>
-        prod.tipoOperacao === TipoOperacao.Gasto ? prod.valor : 0
-      )
-      .reduce((total, valor) => total! + valor!);
+    // dispatch(clearDashboard());
+    const result = await api
+      .get(`/api/Operacao/Get/recebimentos/${month}/${year}`)
+      .then((result: AxiosResponse<GetOperacaoDtoListServiceResponse>) => {
+        dispatch(setRecebimentosList(result.data));
+        const recebimentosValue = store
+          .getState()
+          .dashboardStore.recebimentosList.data!.map((prod) =>
+            prod.tipoOperacao === TipoOperacao.Recebimento ? prod.valor : 0
+          )
+          .reduce((total, valor) => total! + valor!);
 
-    dispatch(setGastos(result!));
+        dispatch(setRecebimentos(recebimentosValue!));
+      });
+
     return result;
   };
 }
